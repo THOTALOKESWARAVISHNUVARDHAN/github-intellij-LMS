de3f50212448284ca7fa86e965b044178b113d82
THOTALOKESWARAVISHNUVARDHAN <132439271+THOTALOKESWARAVISHNUVARDHAN@users.noreply.github.com>
2026-05-07 07:58:53 +0530

    init commit

---
+++ src/main/java/demo/example/githubintellijlms/GithubIntellijLmsApplication.java
@@ -0,0 +1,13 @@
+package demo.example.githubintellijlms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubIntellijLmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubIntellijLmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/demo/example/githubintellijlms/GithubIntellijLmsApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.example.githubintellijlms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubIntellijLmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
